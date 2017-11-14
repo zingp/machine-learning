d8d3c6e22640a2e3cdf898f3004d3923314867a6
ZingP <605156398@qq.com>
2017-11-14 14:04:04 +0800

    mpl

--- matplotlib学习/正弦函数.py
+++ matplotlib学习/正弦函数.py
@@ -17,7 +17,20 @@ y = np.sin(x)
 # plt.plot(x, y, 'r--', x, y, 'go', linewidth=2, markersize=4, label='sin(x)')
 # 'r-'实线；'r--'虚线
 plt.plot(x, y, 'r-', linewidth=2, label='sin(x)')
-plt.plot(x, y, 'bo', markersize=4, label='样本点')
+plt.plot(x, y, 'bo', markersize=1, label='样本点')
 plt.grid(True)  # 显示网格
-plt.legend(loc='upper right')  # 显示图例
+# plt.legend(loc='upper right')  # 显示图例
+plt.legend(loc=0)  # 显示图例
+# 其中loc有很多参数
+# 'best': 0,       自动分配图例到最佳位置
+# 'upper right': 1,
+# 'upper left': 2,
+# 'lower left': 3,
+# 'lower right': 4,
+# 'right': 5,
+# 'center left': 6,
+# 'center right': 7,
+# 'lower center': 8,
+# 'upper center': 9,
+# 'center': 10,
 plt.show()
