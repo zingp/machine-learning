22bad9fb400b8a62fd11eddf821c7e2c1cc0b9ce
ZingP <605156398@qq.com>
2017-11-16 16:39:10 +0800

    add dot() angle_with()

--- 线性代数/create_vector.py
+++ 线性代数/create_vector.py
@@ -3,14 +3,21 @@
 # Author: "Zing-p"
 # Date: 2017/11/15
 import math
+from decimal import Decimal, getcontext
+
+# Decimal 转换为小数，防止浮点数反三角时失去精度
+getcontext().prec = 30
+
 
 class Vector(object):
+
+    CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
 
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)  # coordinates 坐标
+            self.coordinates = tuple([Decimal(x) for x in coordinates])  # coordinates 坐标
             self.dimension = len(coordinates)      # 维度
         except ValueError:
             raise ValueError('The coordinate must be nonempty')
@@ -29,23 +36,47 @@ class Vector(object):
 
     def times_scalar(self, n):
         """标量×向量"""
-        res = [n*x for x in self.coordinates]
+        res = [Decimal(n)*x for x in self.coordinates]
         return Vector(res)
 
     def magnitude(self):
-        """向量大小"""    # magnitude 大小
+        """向量大小"""
+        # magnitude 大小
         # from functools import reduce
         # square_sum = reduce(lambda x, y: x+y, [i**2 for i in self.coordinates])
         square_sum = sum([i**2 for i in self.coordinates])
-        return math.sqrt(square_sum)
+        return Decimal(math.sqrt(square_sum))
 
     def normalized(self):
         """求单位向量"""
         # normalized 规格化
         try:
-            return self.times_scalar(1 / self.magnitude())
+            return self.times_scalar(Decimal('1.0') / self.magnitude())
         except ZeroDivisionError:
-            raise Exception('Cannot normalize the zero vector')
+            raise Exception(self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG)
+
+    def dot(self, v):
+        """点积"""
+        square_list = [x*y for x, y in zip(self.coordinates, v.coordinates)]
+        return sum(square_list)
+
+    def angle_with(self, v, in_degrees=False):
+        # angle 角；degrees 度数, 默认弧度
+        try:
+            u1 = self.normalized()
+            u2 = v.normalized()
+            angle_in_radians = math.acos(u1.dot(u2))
+
+            if in_degrees:
+                angle_per_radians = 180. / math.pi
+                return angle_in_radians * angle_per_radians
+            else:
+                return angle_in_radians
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception('Cannot product an angle with the zero vector')
+            else:
+                raise e
 
     def __str__(self):
         return "Vector: {}".format(self.coordinates)
@@ -87,4 +118,21 @@ if __name__ == '__main__':
     print(vector9.normalized())
     vector10 = Vector([1.996, 3.108, -4.554])
     print(vector10.normalized())
+
+    # 测试点积
+    v = Vector([7.887, 4.138])
+    w = Vector([-8.802, 6.776])
+    print("v.w:", v.dot(w))
+
+    v = Vector([-5.955, -4.904, -1.874])
+    w = Vector([-4.496, -8.755, 7.103])
+    print("v.w:", v.dot(w))
+
+    v = Vector([3.183, -7.627])
+    w = Vector([-2.668, 5.319])
+    print("v w 夹角 弧度：", v.angle_with(w))
+
+    v = Vector([7.35, 0.221, 5.188])
+    w = Vector([2.751, 8.259, 3.985])
+    print("v w 夹角 度数：", v.angle_with(w, in_degrees=True))
 
--- 线性代数/my_test.py
+++ 线性代数/my_test.py
@@ -7,7 +7,16 @@ import math
 from functools import reduce
 a = [1, 2, 3, ]
 b = [4, 5, 6, ]
-for x, y in zip(a, b):
-    print(x, y)
+# for x, y in zip(a, b):
+#     print(x, y)
 # print(math.sqrt(9.9))
-print(reduce(lambda x, y: x+y, [i**2 for i in a]))
+# print(reduce(lambda x, y: x+y, [i**2 for i in a]))
+
+
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
+
+t = Decimal(1.0)
+print(type(t))
