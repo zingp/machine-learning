45579ace244575386292df3947e7b4e8308832b8
ZingP <605156398@qq.com>
2018-10-20 19:37:31 +0800

    rss

--- NaiveBayes/bayes_rss.py
+++ NaiveBayes/bayes_rss.py
@@ -5,3 +5,12 @@ url = "http://www.nasa.gov/rss/dyn/image_of_the_day.rss"
 ny = feedparser.parse(url)
 print(ny['entries'])
 print(len(ny['entries']))
+
+
+def clac_most_freq(vocab_list, fulltext):
+    import operator
+    freq_dic = {}
+    for tocken in vocab_list:
+        freq_dic[tocken] = fulltext.count(tocken)
+    sorted_freq = sorted(freq_dic.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_freq[:30]
