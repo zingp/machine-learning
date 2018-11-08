b2aa8fa43251c890e5fc1426b5bc755ce71d9ff9
ZingP <605156398@qq.com>
2018-11-08 16:18:50 +0800

    最小二乘法

--- Least_squares/least_squares.py
+++ Least_squares/least_squares.py
@@ -4,5 +4,7 @@ import numpy as np
 # print(np.random.normal(9, 1))
 
 # print(np.mat([[x, 1] for x in range(10)]))
-print(np.mat('3,6').T)
-print(np.mat('1,2; 4,5'))
+# print(np.mat('3,6').T)
+# print(np.mat('1,2; 4,5'))
+print(np.matrix([1, 2]).T)
+print(np.mat([1, 2]).T)
